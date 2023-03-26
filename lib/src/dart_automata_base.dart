c2e55e71308b0296cd45b9e49057837afccfa73d
Lane Schwartz <dowobeha@gmail.com>
2023-03-26 13:51:28 -1000

    Basic classes

--- lib/src/dart_automata_base.dart
+++ lib/src/dart_automata_base.dart
@@ -4,3 +4,54 @@
 class Awesome {
   bool get isAwesome => true;
 }
+
+
+class State {
+  
+}
+
+class Label {
+  final List<String> value;
+
+  Label(this.value);
+
+  Label.fromInput(String inputWord) : value = [inputWord];
+
+  Label.fromPair(String inputWord, String outputWord) : value = [inputWord, outputWord];
+}
+
+class LogProbability {
+  final double value;
+
+  LogProbability(this.value);
+
+  static final multiplicative_identity = LogProbability(0.0);  // zero
+  static final additive_identity = LogProbability(-1.0 / 0.0); // -inf
+
+  LogProbability.probabilityOne() : value = 0.0;
+  
+  LogProbability operator +(LogProbability other) {
+    if (this.value >= other.value) {
+      return LogProbability(this.value);
+    } else {
+      return LogProbability(other.value);
+    }
+  }
+
+  LogProbability operator *(LogProbability other) {
+    return LogProbability(this.value + other.value);
+  }
+}
+
+class Transition {
+
+  final Label label;
+  final LogProbability weight;
+  final State target;
+  
+  Transition(this.label, this.weight, this.target);
+}
+
+class FST {
+
+}
