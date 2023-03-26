7c6135094c5ab530508c11b6aab672855a25784a
Lane Schwartz <dowobeha@gmail.com>
2023-03-26 13:07:15 -1000

    Initial commit

---
+++ test/dart_automata_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_automata/dart_automata.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
